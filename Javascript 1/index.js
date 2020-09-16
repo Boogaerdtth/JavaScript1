289deaba9a0ad3df8cc0410fc4c4bacdfab96108
Boogaerdtth <70010486+Boogaerdtth@users.noreply.github.com>
2020-09-16 13:00:48 +0200

    Add files via upload

---
+++ Javascript 1/index.js
@@ -0,0 +1,16 @@
+console.log('Hello Winc Academy')
+let name = 'Jasper';
+console.log(name);
+
+console.log(4 + 4);
+console.log('4' + '4');
+console.log(30 + 1);
+console.log(16 - 4);
+console.log('16' - '4');
+console.log(3 * 3);
+console.log(16 / 4);
+console.log(40 % 4);
+console.log(41 % 4);
+console.log(39 % 4);
+let age = 29
+console.log(age);
